faf49789ced7f268c1bbc17de9914fe610577cef
Péter Szilvási <peti.szilvasi95@gmail.com>
2020-04-30 13:53:03 +0200

    Create Interface IWizard interface for unit Test

---
+++ ProgrammingContest/IWizard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingContest
+{
+    interface IWizard
+    {
+        double GetThreeSheepAngle(int y, int x, int z);
+
+        double[] GetSheepsMaximumEyeMovoment(int numberOfSheep);
+
+        int GetAtLeastEyeMovomentSheep(double[] sheepEyeMovoments);
+
+        int[,] SheepCoordinates { get; set; }
+
+        int numberOfSheep { get; set; }
+    }
+}
