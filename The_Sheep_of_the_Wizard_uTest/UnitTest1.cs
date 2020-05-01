1ae2b426b2fe156109c1a15ae8fe60d9172e4602
Péter Szilvási <peti.szilvasi95@gmail.com>
2020-05-01 13:06:39 +0200

    Add unit test project

---
+++ The_Sheep_of_the_Wizard_uTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace The_Sheep_of_the_Wizard_uTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
