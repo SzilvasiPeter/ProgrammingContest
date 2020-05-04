9b1f032d62fc7fbfb0f388badad0f5769a2e55f7
Péter Szilvási <peti.szilvasi95@gmail.com>
2020-05-04 09:29:27 +0200

    Add more testcases to cover 'if' branches.

--- The_Sheep_of_the_Wizard_uTest/WizardTests.cs
+++ The_Sheep_of_the_Wizard_uTest/WizardTests.cs
@@ -75,5 +75,89 @@ namespace The_Sheep_of_the_Wizard_uTest
             Assert.AreEqual(secondSheepAngle, 90, 0.001);
             Assert.AreEqual(thirdSheepAngle, 45, 0.001);
         }
+
+        [Test]
+        public void GetSheepsMaximumEyeMovoment_FourSheep_OneSheep180Angle()
+        {
+            // Arrange
+            Wizard threeSheepWizard = new Wizard();
+            threeSheepWizard.NumberOfSheep = 4;
+            threeSheepWizard.SheepCoordinates = new int[,]
+            {
+                {0, 0},
+                {2, 2},
+                {1, 1},
+                {4, 0}
+            };
+
+            // Act
+            double[] maxAngleOfSheeps = threeSheepWizard.GetSheepsMaximumEyeMovoment();
+
+            // Assert
+            Assert.AreEqual(maxAngleOfSheeps[0], 45, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[1], 90, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[2], 180, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[3], 45, 0.01);
+        }
+
+        [Test]
+        public void GetSheepsMaximumEyeMovoment_TenSheep_ReturnMaxEyeMovoment()
+        {
+            // Arrange
+            Wizard threeSheepWizard = new Wizard();
+            threeSheepWizard.NumberOfSheep = 10;
+            threeSheepWizard.SheepCoordinates = new int[,]
+            {
+                {1, 0},
+                {0, 0},
+                {1, 1},
+                {10, 0},
+                {1, -1},
+                {6, 2},
+                {4, 3},
+                {8, 2},
+                {6, 0},
+                {13, 8}
+            };
+
+            // Act
+            double[] maxAngleOfSheeps = threeSheepWizard.GetSheepsMaximumEyeMovoment();
+
+            // Assert
+            Assert.AreEqual(maxAngleOfSheeps[0], 180, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[1], 90, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[2], 168.690, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[3], 116.892, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[4], 128.659, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[5], 179.999, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[6], 175.364, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[7], 174.805, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[8], 180, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[9], 40.389, 0.01);
+        }
+
+        [Test]
+        public void GetSheepsMaximumEyeMovoment_FourSheep_SameEyeMovoment()
+        {
+            // Arrange
+            Wizard threeSheepWizard = new Wizard();
+            threeSheepWizard.NumberOfSheep = 4;
+            threeSheepWizard.SheepCoordinates = new int[,]
+            {
+                {0, 0},
+                {0, 2},
+                {2, 2},
+                {2, 0}
+            };
+
+            // Act
+            double[] maxAngleOfSheeps = threeSheepWizard.GetSheepsMaximumEyeMovoment();
+
+            // Assert
+            Assert.AreEqual(maxAngleOfSheeps[0], 90, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[1], 90, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[2], 90, 0.01);
+            Assert.AreEqual(maxAngleOfSheeps[3], 90, 0.01);
+        }
     }
 }
